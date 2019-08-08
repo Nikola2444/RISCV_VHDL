e47c1be523e8f3f3a0e961212499f75db6611dde
fouste <djordjemiseljic@gmail.com>
2019-08-08 14:38:07 +0200

    test bench name changes, utils_pkg removed, added BRAM.vhd

--- RV32IMA/RISCV_tb/TOP_RISCV_tb.vhd
+++ RV32IMA/RISCV_tb/TOP_RISCV_tb.vhd
@@ -4,7 +4,6 @@ use ieee.numeric_std.all;
 use ieee.std_logic_unsigned.all;
 use std.textio.all;
 use work.txt_util.all;
-use work.util_pkg.all;
 
 entity TOP_RISCV_tb is
 -- port ();
@@ -13,7 +12,7 @@ end entity;
 
 architecture Behavioral of TOP_RISCV_tb is
    -- file operands
-   constant NUM_COL: integer := 2;   -- number of column of file
+   constant NUM_COL: integer := 2;   -- number of columns of file
    file RISCV_instructions: text open read_mode is "/home/nikola/Documents/git_repos/RISCV_VHDL/RV32IMA/RISCV_tb/assembly_code.txt";
    type t_integer_array       is array(integer range <> )  of integer;
    signal instruction_read: std_logic_vector(31 downto 0);
@@ -29,57 +28,62 @@ architecture Behavioral of TOP_RISCV_tb is
    signal write_ext_data_o   : std_logic_vector(31 downto 0);
 
    --Instruction_mem_signals
-   signal pi_clka_s,pi_clkb_s,pi_ena_s,pi_enb_s,pi_wea_s,pi_web_s:std_logic;
-   signal pi_addra_s,pi_addrb_s: std_logic_vector(9 downto 0);
-   signal pi_dia_s,pi_dib_s:std_logic_vector(31 downto 0);
-   signal po_doa_s,po_dob_s:std_logic_vector(31 downto 0);
-   -- pi_addra extension bacause 32 bit address for instruction mem isn't supported
-   constant address_extend: std_logic_vector (21 downto 0):=(others => '0');
-   signal pi_addrb_s_extended:std_logic_vector(31 downto 0);
+   signal ena_instr_s,enb_instr_s,wea_instr_s,web_instr_s: std_logic;
+   signal addra_instr_s,addrb_instr_s: std_logic_vector(9 downto 0);
+   signal dia_instr_s,dib_instr_s:std_logic_vector(31 downto 0);
+   signal doa_instr_s,dob_instr_s:std_logic_vector(31 downto 0);
    --Data_mem_signals
-   signal pi_clka_s_d,pi_clkb_s_d,pi_ena_s_d,pi_enb_s_d,pi_wea_s_d,pi_web_s_d:std_logic;
-   signal pi_addra_s_d,pi_addrb_s_d: std_logic_vector(9 downto 0);
-   signal pi_dia_s_d,pi_dib_s_d:std_logic_vector(31 downto 0);
-   signal po_doa_s_d,po_dob_s_d:std_logic_vector(31 downto 0);
+   signal ena_data_s,enb_data_s,wea_data_s,web_data_s:std_logic;
+   signal addra_data_s,addrb_data_s: std_logic_vector(9 downto 0);
+   signal dia_data_s,dib_data_s:std_logic_vector(31 downto 0);
+   signal doa_data_s,dob_data_s:std_logic_vector(31 downto 0);
 
-   -- pi_addra extension bacause 32 bit address for data mem isn't supported
-   signal pi_addra_s_d_extended:std_logic_vector(31 downto 0);
+   -- addrb extension bacause 32 bit address for instruction mem isn't supported
+   signal addrb_instr_extended_s:std_logic_vector(31 downto 0);
+   -- addra extension bacause 32 bit address for data mem isn't supported
+   signal addra_data_extended_s:std_logic_vector(31 downto 0);
+
 begin
-   pi_addrb_s_extended <= address_extend & pi_addrb_s;
-   pi_addra_s_d_extended <= address_extend & pi_addra_s_d;
-   -- memories always enabled
-   instruction_mem: entity work.BRAM
+
+   addrb_instr_s <= addrb_instr_extended_s(9 downto 0);
+   addra_data_s  <= addra_data_extended_s(9 downto 0);
+
+   -- Instruction Memory
+   -- PORT A : test bench instruction initializationa
+   -- PORT B : 
+   instruction_mem: entity work.BRAM(behavioral)
       generic map(WADDR => 10,
                   WDATA => 32)
-      port map (pi_clka => clk,
-                pi_clkb => clk,
-                pi_ena => '1',    -- memorie always enabled
-                pi_enb => '1',
-                pi_wea => pi_wea_s,
-                pi_web => pi_web_s,
-                pi_addra => pi_addra_s,
-                pi_addrb => pi_addrb_s,
-                pi_dia => pi_dia_s,
-                pi_dib => pi_dib_s,
-                po_doa => po_doa_s,
-                po_dob => po_dob_s);
+      port map (clk_a => clk,
+                clk_b => clk,
+                en_a_i => '1',    -- memory always enabled
+                en_b_i => '1',
+                we_a_i => wea_instr_s,
+                we_b_i => web_instr_s,
+                addr_a_i => addra_instr_s,
+                addr_b_i => addrb_instr_s,
+                data_a_i => dia_instr_s,
+                data_b_i => dib_instr_s,
+                data_a_o => doa_instr_s,
+                data_b_o => dob_instr_s);
 
 
-   data_mem: entity work.BRAM
+   -- Data memory
+   data_mem: entity work.BRAM(behavioral)
       generic map(WADDR => 10,
                   WDATA => 32)
-      port map (pi_clka => clk,
-                pi_clkb => clk,
-                pi_ena => '1',    -- memorie always enabled
-                pi_enb => '1',
-                pi_wea => pi_wea_s_d,
-                pi_web => pi_web_s_d,
-                pi_addra => pi_addra_s_d,
-                pi_addrb => pi_addrb_s_d,
-                pi_dia => pi_dia_s_d,
-                pi_dib => pi_dib_s_d,
-                po_doa => po_doa_s_d,
-                po_dob => po_dob_s_d);
+      port map (clk_a=> clk,
+                clk_b => clk,
+                en_a_i => '1',    -- memory always enabled
+                en_b_i => '1',
+                we_a_i => wea_data_s,
+                we_b_i => web_data_s,
+                addr_a_i => addra_data_s,
+                addr_b_i => addrb_data_s,
+                data_a_i => dia_data_s,
+                data_b_i => dib_data_s,
+                data_a_o => doa_data_s,
+                data_b_o => dob_data_s);
 
 
    --******TOP_RISCV instance**********************
@@ -89,30 +93,31 @@ begin
       port map (
          clk                => clk,
          reset              => reset,
-         instruction_i      => po_dob_s,
-         pc_o               => pi_addrb_s_extended,         
-         mem_ext_write_o    => pi_wea_s_d,
-         ext_data_address_o => pi_addra_s_d_extended,
-         read_ext_data_i    => po_dob_s_d,
-         write_ext_data_o   => po_dob_s_d);
+         instruction_i      => dob_instr_s,
+         pc_o               => addrb_instr_extended_s,         
+         mem_ext_write_o    => wea_data_s,
+         ext_data_address_o => addra_data_extended_s,
+         read_ext_data_i    => dob_data_s,
+         write_ext_data_o   => dob_data_s);
    
    --******Filling instruction MEM*****************
+   --
    read_file_proc:process
       variable row: line;
       variable i: integer:= 0;
    begin
-      pi_ena_s <= '1';
-      pi_wea_s <= '1';      
+      ena_instr_s <= '1';
+      wea_instr_s <= '1';      
       while (not endfile(RISCV_instructions))loop         
          readline(RISCV_instructions, row);
-         pi_addra_s <= std_logic_vector(to_unsigned(i, 10));
-         pi_dia_s <= to_std_logic_vector(string(row));
+         addra_instr_s <= std_logic_vector(to_unsigned(i, 10));
+         dia_instr_s <= to_std_logic_vector(string(row));
          instruction_read <= to_std_logic_vector(string(row));
          i := i + 1;
          wait until rising_edge(clk);
       end loop;
-      pi_ena_s <= '0';
-      pi_wea_s <= '0';
+      ena_instr_s <= '0';
+      wea_instr_s <= '0';
       wait;
    end process;
    --****************************************************
