71a09c7539a82ffa291ef76840460fc18a1333f3
fouste <djordjemiseljic@gmail.com>
2019-08-10 14:09:20 +0200

    Register bank reworked, error found and corrected

--- RV32IMA/data_path/register_bank.vhd
+++ RV32IMA/data_path/register_bank.vhd
@@ -23,31 +23,38 @@ end entity;
 
 architecture Behavioral of register_bank is
   type reg_bank is array  (0 to 31) of std_logic_vector(31 downto 0);
-  signal reg_bank_i: reg_bank := (others => (others => '0'));
+  signal reg_bank_s: reg_bank;
   
 begin
 
-  reg_bank_write: process (clk) is
-  begin
-    if (falling_edge(clk))then      
-        if (reg_write_i = '1') then
-          reg_bank_i(to_integer(unsigned(write_reg_i))) <= write_data_i;
-        end if;
+   reg_bank_write: process (clk) is
+      begin
+      if (falling_edge(clk))then      
+         if (reset = '0')then
+            reg_bank_s <= (others => (others => '0'));
+         elsif (reg_write_i = '1') then
+            reg_bank_s(to_integer(unsigned(write_reg_i))) <= write_data_i;
+         end if;
       end if;      
-  end process;
+   end process;
 
-  reg_bank_read: process (clk) is
-  begin
-    if (rising_edge(clk))then
-      if (reset = '0')then
-        reg_bank_i <= (others => (others => '0'));
-        read_data1_o <= (others => '0');
-        read_data2_o <= (others => '0');
-      else
-        read_data1_o <= reg_bank_i(to_integer(unsigned(read_reg1_i)));
-        read_data2_o <= reg_bank_i(to_integer(unsigned(read_reg2_i)));
-      end if;
-    end if;          
-  end process;
-  
+   reg_bank_read: process (clk) is
+   begin
+      if (rising_edge(clk))then
+
+         if(to_integer(unsigned(read_reg1_i))=0) then
+            read_data1_o <= std_logic_vector(to_unsigned(0,WIDTH));
+         else
+            read_data1_o <= reg_bank_s(to_integer(unsigned(read_reg1_i)));
+         end if;
+
+         if(to_integer(unsigned(read_reg2_i))=0) then
+            read_data1_o <= std_logic_vector(to_unsigned(0,WIDTH));
+         else
+            read_data2_o <= reg_bank_s(to_integer(unsigned(read_reg2_i)));
+         end if;
+
+      end if;          
+   end process;
+
 end architecture;
