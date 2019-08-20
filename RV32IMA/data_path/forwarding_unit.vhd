13b9e096072a8542f9154ba084743f0016afab58
Nikola <krajka95@gmail.com>
2019-08-20 14:21:33 +0200

    forwarding unit and hazard unit updated, and they now support branch instructions

--- RV32IMA/data_path/forwarding_unit.vhd
+++ RV32IMA/data_path/forwarding_unit.vhd
@@ -17,20 +17,33 @@ entity forwarding_unit is
       id_ex_reg_rs1: in std_logic_vector(4 downto 0);
       id_ex_reg_rs2: in std_logic_vector(4 downto 0);
 
+      --if_id inputs
+      if_id_reg_rs1: in std_logic_vector(4 downto 0);
+      if_id_reg_rs2: in std_logic_vector(4 downto 0);
       --forward control outputs
       forward_a: out std_logic_vector (1 downto 0);
-      forward_b: out std_logic_vector(1 downto 0)                  
+      forward_b: out std_logic_vector(1 downto 0);
+      --forward control outputs
+      --They control multiplexers infront of equality test
+      forward_branch_a: out std_logic_vector (1 downto 0); -- mux a 
+      forward_branch_b: out std_logic_vector(1 downto 0)-- mux b
       );
 end entity;
 
 architecture Behavioral of forwarding_unit is
    constant zero_c: std_logic_vector (31 downto 0) := std_logic_vector(to_unsigned(0, 32));
 begin
+
+   
+   --process that checks whether forwarding for instructions in EX stage is needed or not.
+   -- forwarding from MEM stage has advantage over forwading information from WB
+   -- stage, because information contained there is more fresh than in WB.
    forward_proc:process(ex_mem_reg_write, ex_mem_reg_rd, mem_wb_reg_write, mem_wb_reg_rd,
                         id_ex_reg_rs1, id_ex_reg_rs2)is
    begin
       forward_a <= "00";
       forward_b <= "00";
+      -- forwarding from WB stage
       if (mem_wb_reg_write = '1' and mem_wb_reg_rd /= zero_c)then
          if (mem_wb_reg_rd = id_ex_reg_rs1)then
             forward_a <= "01";
@@ -38,12 +51,41 @@ begin
             forward_b <= "01";            
          end if;   
       end if;
+      -- forwarding from MEM stage
       if (ex_mem_reg_write = '1' and ex_mem_reg_rd /= zero_c)then
          if (ex_mem_reg_rd = id_ex_reg_rs1)then
             forward_a <= "10";
          elsif (ex_mem_reg_rd = id_ex_reg_rs2)then
             forward_b <= "10";
          end if;
+      end if;      
+   end process;
+
+
+   --process that checks whether forwarding is needed for branch instructions in
+   --ID stage or not.
+   -- forwarding from MEM stage has advantage over forwading information from WB
+   -- stage, because information contained there is more fresh than in WB.
+   forward_branch_proc:process(ex_mem_reg_write, ex_mem_reg_rd, mem_wb_reg_write, mem_wb_reg_rd,
+                               if_id_reg_rs1, if_id_reg_rs2)is
+   begin
+      forward_branch_b <= "00";
+      forward_branch_a <= "00";
+      -- forwarding from WB stage
+      if (mem_wb_reg_write = '1' and mem_wb_reg_rd /= zero_c)then
+         if (mem_wb_reg_rd = if_id_reg_rs1)then
+            forward_branch_a <= "01";
+         elsif(mem_wb_reg_rd = if_id_reg_rs2)then
+            forward_branch_b <= "01";
+         end if;   
       end if;
+      -- forwarding from MEM stage
+      if (ex_mem_reg_write = '1' and ex_mem_reg_rd /= zero_c)then
+         if (ex_mem_reg_rd = if_id_reg_rs1)then
+            forward_branch_a <= "10";
+         elsif (ex_mem_reg_rd = if_id_reg_rs2)then
+            forward_branch_b <= "10";
+         end if;
+      end if;      
    end process;
 end architecture;
--- RV32IMA/data_path/hazard_unit.vhd
+++ RV32IMA/data_path/hazard_unit.vhd
@@ -4,39 +4,85 @@ use ieee.numeric_std.all;
 
 entity hazard_unit is
    port (
+      clk: in std_logic;
+      reset : in std_logic;
+      
+      id_ex_reg_write_i: in std_logic;
+      id_ex_mem_read_i:in std_logic;
+      id_ex_reg_rd_i: in std_logic_vector(4 downto 0);
 
-      id_ex_mem_read:in std_logic;
-      id_ex_reg_rd: in std_logic_vector(4 downto 0);
+      if_id_reg_rs1_i: in std_logic_vector(4 downto 0);
+      if_id_reg_rs2_i: in std_logic_vector(4 downto 0);
 
-      if_id_reg_rs1: in std_logic_vector(4 downto 0);
-      if_id_reg_rs2: in std_logic_vector(4 downto 0);
-
+      -- inputs needed to deal with branch data hazards
+      branch_id_i: in std_logic_vector(1 downto 0);
       --control outputs
-      pc_write: out std_logic;--controls program counter
-      if_dwrite: out std_logic;--controls istruction fetch 
-      control_stall: out std_logic -- controls mux that sets all the control
-                                   -- signals to zero if stall is needed
+      pc_write_o: out std_logic;--controls program counter
+      if_dwrite_o: out std_logic;--controls istruction fetch 
+      control_stall_o: out std_logic -- controls mux that sets all the control
+    -- signals to zero if stall is needed
       );
 end entity;
 
+
 architecture behavioral of hazard_unit is
+   type stall_enum is (stall_1_clk, stall_2_clk);
+   signal state_reg, state_next: stall_enum;
+   signal control_group_s:std_logic_vector(2 downto 0):=(others =>'0');
 begin
-   hazard_proc:process(id_ex_mem_read, id_ex_reg_rd,
-                       if_id_reg_rs1, if_id_reg_rs2)is
+   process (clk)is
    begin
-      pc_write <= '0';
-      if_dwrite <= '0';
-      control_stall <= '0';
-      if (id_ex_mem_read = '1')then
-         if(id_ex_reg_rd = if_id_reg_rs1)then
-            pc_write <= '1';
-            if_dwrite <= '1';
-            control_stall <= '1';
-         elsif(id_ex_reg_rd = if_id_reg_rs2)then
-            pc_write <= '1';
-            if_dwrite <= '1';
-            control_stall <= '1';
+      if (rising_edge(clk))then
+         if (reset = '0') then
+            state_reg <= stall_1_clk;
+         else
+            state_reg <= state_next;
          end if;
       end if;
-   end process;      
+   end process;
+   
+   process (id_ex_mem_read_i, id_ex_reg_rd_i, id_ex_reg_write_i,
+            if_id_reg_rs1_i, if_id_reg_rs2_i, branch_id_i, state_reg, state_next)is
+   begin
+      control_group_s <= (others =>'0');
+      state_next <= state_reg;
+
+      case state_reg is
+         when stall_1_clk=>
+            --non load instruction in EX, but in ID is a branch instruction. 1
+            --clk stall maybe needed
+            if(id_ex_reg_write_i = '1' and id_ex_mem_read_i = '0')then
+               if (id_ex_reg_rd_i = if_id_reg_rs1_i and (branch_id_i = "01" or branch_id_i = "11"))then
+                  control_group_s <= (others =>'1');
+               elsif (id_ex_reg_rd_i = if_id_reg_rs2_i and branch_id_i = "01")then
+                  control_group_s <= (others =>'1');
+               end if;
+               state_next <= stall_1_clk;
+            end if;
+            --load instruction in EX and branch instruction in ID. 2 clk stall
+            --maybe needed
+            if(id_ex_mem_read_i = '1' and branch_id_i /= "10")then
+               control_group_s <= (others =>'1');
+               state_next <= stall_2_clk;
+            end if;
+
+            --load in EX, but in ID is a non branch instruction. 1
+            --clk stall maybe needed
+            if (id_ex_mem_read_i = '1' and branch_id_i = "00")then               
+               if((id_ex_reg_rd_i = if_id_reg_rs1_i) or (id_ex_reg_rd_i = if_id_reg_rs2_i))then
+                  control_group_s <= (others =>'1');
+               end if;
+               state_next <= stall_1_clk;
+            end if;
+
+         when stall_2_clk=> 
+            control_group_s <= (others =>'1');
+            state_next <= stall_1_clk;
+      end case;
+   end process;
+
+   pc_write_o <= control_group_s(0);
+   if_dwrite_o <= control_group_s(1);
+   control_stall_o <= control_group_s(2);
+               
 end architecture;
