80c95d68859131e01e1139c79655fed2dbe06ed3
fouste <djordjemiseljic@gmail.com>
2019-09-04 23:44:09 +0200

    functions for converting unary to binary added, who tf knows if they actually work, will test after next commit

--- RV32IMA/data_path/ALU/ALU_ft.vhd
+++ RV32IMA/data_path/ALU/ALU_ft.vhd
@@ -5,15 +5,14 @@ USE work.ft_pkg.all;
 
 ENTITY ALU_ft IS
    GENERIC(
-      NUM_MODULES: NATURAL := 3;
-      WIDTH : NATURAL := 32);
+      NUM_MODULES: NATURAL := 3);
    PORT(
       clk  :  IN   STD_LOGIC;
       reset  :  IN   STD_LOGIC;
-      a_i  :  IN   STD_LOGIC_VECTOR(WIDTH-1 DOWNTO 0); 
-      b_i  :  IN   STD_LOGIC_VECTOR(WIDTH-1 DOWNTO 0);
+      a_i  :  IN   STD_LOGIC_VECTOR(31 DOWNTO 0); 
+      b_i  :  IN   STD_LOGIC_VECTOR(31 DOWNTO 0);
       op_i  :  IN   STD_LOGIC_VECTOR(4 DOWNTO 0); 
-      res_o   :  OUT  STD_LOGIC_VECTOR(WIDTH-1 DOWNTO 0); 
+      res_o   :  OUT  STD_LOGIC_VECTOR(31 DOWNTO 0); 
       zero_o   :  OUT  STD_LOGIC;
       of_o   :  OUT  STD_LOGIC);
 END ALU_ft;
@@ -24,7 +23,8 @@ END ALU_ft;
 ARCHITECTURE behavioral OF ALU_ft IS
    
    
-   signal alu_array_s : multi32_t (0 to NUM_MODULES);
+   signal alu_res_array_s : array32_t (0 to NUM_MODULES-1);
+   signal alu_sw_array_s : array32_t (0 to NUM_MODULES-1);
    signal alu_valid_reg,alu_valid_next,alu_valid_s: std_logic_vector(31 downto 0);
    signal zero_s: std_logic_vector(NUM_MODULES downto 0);
    signal of_s: std_logic_vector(NUM_MODULES downto 0);
@@ -39,13 +39,25 @@ BEGIN
       generic map (WIDTH => WIDTH)
       port map (a_i => a_i,
                 b_i => b_i,
-                res_s => alu_array_s(i),
+                res_s => alu_res_array_s(i),
                 zero_o => zero_s(i),
                 of_o => of_s(i));
 
+      alu_sw_array(i) <= alu_res_array(i) when alu_valid_reg(i) = '0' else
+                      (others => '0');
+
+      alu_valid_s(i) <= '1' when (IZLAZ_VOTERA xor REZULTAT_OVOG_ALUA) = (others => '0') else -- TODO nadji prave signale
+                        '0';
+
+   switch_logic:
+   process is
+   begin
+      
+   end process;
 
    end generate instantiate_alus;
 
+   entity work.treshold_voter(behavioral) -- TODO instanciraj voter
 
    reg: process (clk) is
    begin
--- RV32IMA/packages/ft_pkg.vhd
+++ RV32IMA/packages/ft_pkg.vhd
@@ -1,8 +1,53 @@
 library ieee;
 use ieee.std_logic_1164.all;
+USE ieee.math_real.ceil;
+USE ieee.math_real.log2;
 
 package ft_pkg is
 
-   type multi32_t is array (integer range <>) of std_logic_vector(31 downto 0);
+   type array32_t is array (integer range <>) of std_logic_vector(31 downto 0);
+   
+   
+   -- counts number of ones in std_logic_vector and returns it in binary (unary to binary converter)
+   -- serial -> function is implemented as a simple loop, good readability bad performance
+   function count_ones_serial    (vector : std_logic_vector) return std_logic_vector;
+   -- recursive -> function is implemented as a binary tree in hope of better timing result
+   function count_ones_recursive (vector : std_logic_vector) return std_logic_vector;
 
 end package ft_pkg;
+
+
+
+
+package body ft_package is
+
+
+   function count_ones_serial (vector : std_logic_vector) return std_logic_vector is
+      constant RETURN_W : integer := integer(ceil(log2(real(vector'length)))+1);
+      variable count : std_logic_vector(RETURN_W-1 downto 0);
+      
+   begin
+      count := (others => '0');
+      
+      for i in 0 to (vector'length-1) loop
+         count := std_logic_vector(unsigned(count) + to_unsigned(RETURN_W,vector(i)));
+      end loop;
+
+      return count;
+   end count_ones_serial;
+
+   function count_ones_recursive (vector : std_logic_vector) return std_logic_vector is
+      constant RETURN_W : integer := integer(ceil(log2(real(vector'length)))+1);
+      variable count : std_logic_vector(RETURN_W-1 downto 0);
+      
+   begin
+         if(vector'lenght=1)then
+            return vecotr(0);
+         end if;
+
+         count := std_logic_vector(unsigned(count_ones_recursive(vector(vector'length downto vector'length/2))) + unsigned(count_ones_recursive(vector'length/2-1 downto 0)));
+
+         return count;
+   end count_ones_recursive;
+
+end package body ft_package;
