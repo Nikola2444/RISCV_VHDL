7066bd60fcf81c0b5ad9f5922807aa56c3eab918
fouste <djordjemiseljic@gmail.com>
2019-09-05 01:45:19 +0200

    serial function works, recursive, not so much Vivado is spewing mad errors

--- RV32IMA/packages/ft_pkg.vhd
+++ RV32IMA/packages/ft_pkg.vhd
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 USE ieee.math_real.ceil;
 USE ieee.math_real.log2;
 
@@ -12,42 +13,50 @@ package ft_pkg is
    -- serial -> function is implemented as a simple loop, good readability bad performance
    function count_ones_serial    (vector : std_logic_vector) return std_logic_vector;
    -- recursive -> function is implemented as a binary tree in hope of better timing result
-   function count_ones_recursive (vector : std_logic_vector) return std_logic_vector;
+   function count_ones_recursive (vector : std_logic_vector; RETURN_W : natural) return std_logic_vector;
 
 end package ft_pkg;
 
 
 
 
-package body ft_package is
+package body ft_pkg is
 
 
    function count_ones_serial (vector : std_logic_vector) return std_logic_vector is
-      constant RETURN_W : integer := integer(ceil(log2(real(vector'length)))+1);
+      constant RETURN_W : integer := (integer(ceil(log2(real(vector'length)))) + integer(1));
       variable count : std_logic_vector(RETURN_W-1 downto 0);
-      
    begin
+
       count := (others => '0');
-      
+
       for i in 0 to (vector'length-1) loop
-         count := std_logic_vector(unsigned(count) + to_unsigned(RETURN_W,vector(i)));
+         if(vector(i) = '1') then
+            count := std_logic_vector(unsigned(count) + (to_unsigned(1,RETURN_W)));
+         else
+            count := std_logic_vector(unsigned(count) + (to_unsigned(0,RETURN_W)));
+         end if;
       end loop;
 
       return count;
+
    end count_ones_serial;
 
-   function count_ones_recursive (vector : std_logic_vector) return std_logic_vector is
-      constant RETURN_W : integer := integer(ceil(log2(real(vector'length)))+1);
+
+   function count_ones_recursive (vector : std_logic_vector; RETURN_W: natural) return std_logic_vector is
+      --constant RETURN_W : integer := (integer(ceil(log2(real(vector'length)))) + integer(1));
       variable count : std_logic_vector(RETURN_W-1 downto 0);
-      
+      variable res_upper,res_lower : unsigned(RETURN_W-1 downto 0);
    begin
-         if(vector'lenght=1)then
-            return vecotr(0);
+         if(vector'length = 1)then
+            return std_logic_vector(to_unsigned(to_integer(unsigned(vector)),RETURN_W));
          end if;
 
-         count := std_logic_vector(unsigned(count_ones_recursive(vector(vector'length downto vector'length/2))) + unsigned(count_ones_recursive(vector'length/2-1 downto 0)));
+         res_upper := unsigned(count_ones_recursive(vector(vector'length downto vector'length/2),RETURN_W));
+         res_lower := unsigned(count_ones_recursive(vector(vector'length/2-1 downto 0),RETURN_W));
+         count := std_logic_vector(res_upper + res_lower);
 
          return count;
    end count_ones_recursive;
 
-end package body ft_package;
+end package body ft_pkg;
