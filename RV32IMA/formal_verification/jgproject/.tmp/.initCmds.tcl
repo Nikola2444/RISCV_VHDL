881fa0e62b136a22174babdda1912da4cc7bb97d
Nikola <nikola2444@yahoo.com>
2019-09-11 16:27:24 +0200

    deleted unneeded trash

--- RV32IMA/formal_verification/jgproject/.tmp/.initCmds.tcl
+++
@@ -1,3 +0,0 @@
-@puts "INFO: successfully checked out licenses \"jasper_interactive\" and \"jasper_fpv\"."
-@puts "INFO: reading configuration file \"/nethome/nikola.kovacevic/.config/jasper/jaspergold.conf\"."
-@get_message -clear
