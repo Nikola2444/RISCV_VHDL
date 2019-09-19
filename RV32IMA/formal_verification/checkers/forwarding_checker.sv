145e253675419baeb3a735699fc6f9ccffb9944d
Nikola <krajka95@gmail.com>
2019-09-19 16:39:37 +0200

    documentation if pdf form added

--- RV32IMA/formal_verification/checkers/forwarding_checker.sv
+++ RV32IMA/formal_verification/checkers/forwarding_checker.sv
@@ -1,32 +1,32 @@
 module forwarding_checker
   (
-   input logic 		reset,
-   input logic 		clk,
+    input logic 	 reset,
+    input logic 	 clk,
 
-   input logic 		id_ex_flush_i,
-   input logic 		stall,
-  
-   input logic [31 : 0] alu_in_a_i,
-   input logic [31 : 0] alu_in_b_i,
-   input logic [31 : 0] branch_condition_a_id_s,
-   input logic [31 : 0] branch_condition_b_id_s,
+    input logic 	 id_ex_flush_i,
+    input logic 	 stall,
+    
+    input logic [31 : 0] alu_in_a_i,
+    input logic [31 : 0] alu_in_b_i,
+    input logic [31 : 0] branch_condition_a_id_s,
+    input logic [31 : 0] branch_condition_b_id_s,
 
-   input logic [4 : 0] 	rs1_address_id_i,
-   input logic [4 : 0] 	rs2_address_id_i,
-   input logic [4 : 0] 	rd_address_mem_i,
-   input logic [4 : 0] 	rd_address_wb_i,
-   input logic 		reg_write_i,
-  
-   input logic [6 : 0] 	opcode_id_i,
+    input logic [4 : 0]  rs1_address_id_i,
+    input logic [4 : 0]  rs2_address_id_i,
+    input logic [4 : 0]  rd_address_mem_i,
+    input logic [4 : 0]  rd_address_wb_i,
+    input logic 	 reg_write_i,
+   
+    input logic [6 : 0]  opcode_id_i,
 
-   input logic [31 : 0] alu_result_mem_i,
-   input logic [31 : 0] rd_data_wb_i
-  
-   );   
+    input logic [31 : 0] alu_result_mem_i,
+    input logic [31 : 0] rd_data_wb_i
+    
+);   
 
    
-   logic [4 : 0] 	rs1_address_ex_r;
-   logic [4 : 0] 	rs2_address_ex_r;
+   logic [4 : 0] 	 rs1_address_ex_r;
+   logic [4 : 0] 	 rs2_address_ex_r;
    
    
    default clocking @(posedge clk); endclocking
@@ -52,7 +52,7 @@ module forwarding_checker
    
 
    /****************************ASSERTS THAT CHECK FORWARDING TO ALU UNIT**************************/
-   
+  
    //Logic needed to formaly verify forwarding to alu unit inputs
    assign mem_alu_forward_a_check = rs1_address_ex_r == rd_address_mem_i && rd_address_mem_i != 0;
    assign ex_alu_a_opcode_check = $past(opcode_id_i) != 7'b0010111 && $past(opcode_id_i) != 7'b0110111;   
@@ -63,7 +63,7 @@ module forwarding_checker
    assign wb_alu_forward_b_check = rs2_address_ex_r == rd_address_wb_i && rd_address_wb_i != 0;
    
    assign wb_alu_forward_a_check = rs1_address_ex_r == rd_address_wb_i && rd_address_wb_i != 0;
-   
+  
    
    // Asserts that correct value will be forwarded to alu input 'a' from mem stage to ex stage
    mem_alu_a_forward_assert: assert property ((mem_alu_forward_a_check && ex_alu_a_opcode_check) ##1 reg_write_i |-> $past(alu_result_mem_i) == $past(alu_in_a_i ));
@@ -76,12 +76,12 @@ module forwarding_checker
 
    // Asserts that correct value will be forwarded to alu input 'b' from wb stage to ex stage when there was no previous forwarding from mem_stage
    wb_alu_b_forward_assert: assert property ((wb_alu_forward_b_check && ex_alu_b_opcode_check) and (reg_write_i ##1 !reg_write_i) |-> $past(rd_data_wb_i) == $past(alu_in_b_i));
-   
+      
    /**********************************************************************************************/
    
    
    
-   /****************************ASSERTS THAT CHECK FORWARDING TO BRANCH CONDITION*****************/
+/****************************ASSERTS THAT CHECK FORWARDING TO BRANCH CONDITION*****************/
 
    //Logic needed to formaly verify forwarding to branch condition inputs
    assign mem_branch_forward_a_check = rs1_address_id_i == rd_address_mem_i && rd_address_mem_i != 0;
@@ -100,7 +100,7 @@ module forwarding_checker
    
    // Asserts that correct value will be forwarded to branch condition input 'b' from wb stage to id stage
    wb_branch_b_forward_assert:assert property (wb_branch_forward_b_check and (reg_write_i ##1 !reg_write_i) |-> $past(branch_condition_b_id_s) == $past(rd_data_wb_i));
-   
+         
    /**********************************************************************************************/
    
 
